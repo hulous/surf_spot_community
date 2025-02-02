d2adb9fe93fbbacc2a00557089359d8a7b7e697d
Fab <fhulous@gmail.com>
2025-02-02 19:12:55 +0100

    Install and set Devise

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable
+
+  # validator: a user must have a uniq email
+  validates :email, presence: true, uniqueness: true
+end
